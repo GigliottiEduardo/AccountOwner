fd8863eebc9d9733d5bb320728e98fec981b7ec3
Eduardo Gigliotti <99850450+GigliottiEduardo@users.noreply.github.com>
2023-02-23 22:29:27 -0300

    "Teste de Log; Criação do Projeto de biblioteca de classe Entities para as entidades e contexto"

--- backend/AccountOwnerServer/Program.cs
+++ backend/AccountOwnerServer/Program.cs
@@ -4,7 +4,8 @@ using Microsoft.AspNetCore.HttpOverrides;
 
 var builder = WebApplication.CreateBuilder(args);
 
-LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+LogManager.LoadConfiguration(string.Concat(
+    Directory.GetCurrentDirectory(), "/nlog.config"));
 
 // Add services to the container.
 builder.Services.ConfigureCors();
